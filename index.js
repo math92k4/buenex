aaf2226f69619785717cea0c592e905e84391ac9
Mathias <math92k4@stud.kea.dk>
2021-09-10 17:14:39 +0200

    slow follow

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ document.addEventListener('DOMContentLoaded', init)
 function init() {
   initCursor()
 
-  window.addEventListener('mousemove', movePlanet)
+  // window.addEventListener('mousemove', movePlanet)
+  movePlanet()
 }
 
-function movePlanet(event) {
+function movePlanet2(event) {
   const planet = document.querySelector('#globe')
   const planetBC = planet.getBoundingClientRect()
+  console.log(planetBC)
   const planetSize = planet.offsetHeight
   const planetX = planetBC.x + planetSize / 2
   const planetY = planetBC.y + planetSize / 2
@@ -30,4 +32,34 @@ function movePlanet(event) {
   }
   planet.style.setProperty('--new-x', newX)
   planet.style.setProperty('--new-y', newY)
+}
+
+function movePlanet() {
+  const planet = document.querySelector('#globe')
+  const planetWidth = 150
+  const originalX = planet.getBoundingClientRect().x + planetWidth
+  const originalY = planet.getBoundingClientRect().y + planetWidth
+
+  let mouseY = 0
+  let mouseX = 0
+
+  window.addEventListener('mousemove', (e) => {
+    mouseX = (e.clientX - originalX) / 10
+    mouseY = (e.clientY - originalY) / 10
+  })
+  const delay = 100
+  let revisedX = 0
+  let revisedY = 0
+
+  function animatePlanet() {
+    requestAnimationFrame(animatePlanet)
+
+    revisedX += (mouseX - revisedX) / delay
+    revisedY += (mouseY - revisedY) / delay
+
+    planet.style.setProperty('--new-x', revisedX + 'px')
+    planet.style.setProperty('--new-y', revisedY + 'px')
+  }
+
+  animatePlanet()
 }
